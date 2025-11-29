386373eecc7f1a90592a26411db920115568a159
Mikelletto <122797267+Mikelletto@users.noreply.github.com>
2025-11-29 16:49:45 +0100

    Update main.py

--- zad_1/main.py
+++ zad_1/main.py
@@ -1,1 +1,7 @@
-print('Hello word')
+def wyswietl_imiona(lista_imion):
+    for imie in lista_imion:
+        print(imie)
+
+# przykład wywołania
+imiona = ["Ala", "Jan", "Ola", "Piotr", "Kasia"]
+wyswietl_imiona(imiona)
