a31238490aa772c9b1cb1ea964531883bb4ae436
Michal <m.pomada.14@gmail.com>
2025-11-29 17:05:18 +0100

    brakujace

--- zad_2c/main.py
+++ zad_2c/main.py
@@ -1,1 +1,9 @@
-print('test')
+def wyswietl_parzyste(lista_liczb):
+    for liczba in lista_liczb:
+        if liczba % 2 == 0:
+            print(liczba)
+
+
+# przykład użycia:
+liczby = list(range(10))   # 0,1,2,3,4,5,6,7,8,9
+wyswietl_parzyste(liczby)
--- zad_2d/main.py
+++ zad_2d/main.py
@@ -1,1 +1,8 @@
-"print('test')" 
+def wyswietl_co_drugi(lista_liczb):
+    for i in range(0, len(lista_liczb), 2):
+        print(lista_liczb[i])
+
+
+# przykład użycia:
+liczby = list(range(10))
+wyswietl_co_drugi(liczby)
